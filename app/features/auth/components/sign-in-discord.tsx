05a0ddbe500933f271a5715e6c4563d7f7e9218c
Hugo Hsi <hugohsidev@gmail.com>
2025-03-17 17:08:40 -0400

    adds redirect after discord login

--- app/features/auth/components/sign-in-discord.tsx
+++ app/features/auth/components/sign-in-discord.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { authClient } from "../lib/auth-client";
+import { Button } from '@/components/ui/button';
+import { authClient } from '../lib/auth-client';
 
 export default function SignInDiscord() {
 	async function signIn() {
 		const data = await authClient.signIn.social({
-			provider: "discord",
+			provider: 'discord',
+			callbackURL: '/app',
 		});
 	}
 	return <Button onClick={() => signIn()}>Sign in with Discord</Button>;
